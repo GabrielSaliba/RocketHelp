719c5b358f2aff45e11ee0b82949739ea3680131
GabrielSaliba <gabriel.saliba.179@gmail.com>
2022-07-24 17:08:01 -0300

    ✨ - add register screen

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@ import {
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
 import { Loading } from "./src/components/Loading";
+import { Register } from "./src/screens/Register";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -19,7 +20,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Home /> : <Loading />}
+      {fontsLoaded ? <Register /> : <Loading />}
     </NativeBaseProvider>
   );
 }
